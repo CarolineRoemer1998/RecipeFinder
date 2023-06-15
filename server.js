b4461a3c2dc29609775ebf70ed26e8dcad97dc81
Helmni <120278094+Helmni@users.noreply.github.com>
2023-06-15 19:21:57 +0200

    Code aufgeräumt und versucht Sessions zu fixen

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ app.listen(3000, function(){
     console.log("listening on 3000");
 });
 
-//Wenn du den Server nicht starten kannst musst du password-hash runterladen, 
-//mit dem Befehl: npm install password-hash --save
 const passwordHash = require('password-hash');
 
 const session = require('express-session');
@@ -71,18 +69,22 @@ app.post("/onsignup", function(req,res){
     if(_password1==_password2){
         let selectInfo = db.prepare(`SELECT * FROM benutzer WHERE name='${_name}';`).all();
 
-        if(selectInfo.length == 0){
+        if(selectInfo.length == 0)
+        {
             const password = _password1;
             const hash = passwordHash.generate(password);
             db.exec(`INSERT INTO benutzer (name,passwort,email) VALUES ('${_name}', '${hash}', '${_email}');`)
             res.redirect('/home');
             req.session['sessionValue'] = db.prepare(`SELECT id FROM benutzer WHERE name='${_name}';`).all()[0].id;
         }
-        else{
+        else
+        {
+            // 1 = Passworter stimmen nicht überein | 2 = Benutzer existiert bereits
             res.render('signup', {'status': 2});
         }
     }
-    else{
+    else
+    {
         res.render('signup', {'status':1})
     }
 })
@@ -101,20 +103,16 @@ app.post("/onlogin", function(req,res){
         {
             res.redirect('/home');
             req.session['sessionValue'] = db.prepare(`SELECT id FROM benutzer WHERE name='${_name}';`).all()[0].id;
+            console.log(req.session.sessionValue);
         }
         else
         {
             // 1 = Passwort falsch | 2 = Benutzer existiert nicht
-            
-            let _status = 1
-            res.render('login', {'status': _status})
-            console.log("Status: " + _status)
+            res.render('login', {'status': 1})
         }
     }
     else
     {
-        let _status = 2
-        res.render('login', {'status': _status})
-        console.log("Status: " + _status)
+        res.render('login', {'status': 2})
     }
 })
