7a92e372db6baeb56c55ea61ca91b72939ad7417
Helmni <120278094+Helmni@users.noreply.github.com>
2023-06-18 19:09:58 +0200

    Mehr SQL Statements

--- test.sql
+++
@@ -1,3 +0,0 @@
-SELECT zutat.name FROM zutat JOIN rezeptzutat ON zutat.id = zutatid JOIN rezept ON rezeptid = rezept.id WHERE rezept.name = 'Zitronenpasta';
-
-SELECT zutat.name FROM zutat JOIN rezeptzutat ON zutat.id = zutatid JOIN rezept ON rezeptid = rezept.id WHERE rezept.name = 'Avocado-Hühnchen-Salat';
