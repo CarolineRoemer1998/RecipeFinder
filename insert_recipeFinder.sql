aa439c452e9ccbc5e706a7904acd8cae3f4e0ff5
Helmni <120278094+Helmni@users.noreply.github.com>
2023-06-18 13:08:43 +0200

    Mehr Inserts Vorebereitet

--- insert_recipeFinder.sql
+++ insert_recipeFinder.sql
@@ -4,9 +4,6 @@ INSERT INTO zutat (name) VALUES ('Ei');
 INSERT INTO zutat (name) VALUES ('Weizenmehl');
 INSERT INTO zutat (name) VALUES ('Käse');
 INSERT INTO zutat (name) VALUES ('Wasser');
-INSERT INTO zutat (name) VALUES ('Zucker');
-INSERT INTO zutat (name) VALUES ('Salz');
-INSERT INTO zutat (name) VALUES ('Pfeffer');
 INSERT INTO zutat (name) VALUES ('Tomate');
 INSERT INTO zutat (name) VALUES ('Gurke');
 INSERT INTO zutat (name) VALUES ('Kopfsalat');
@@ -15,10 +12,22 @@ INSERT INTO zutat (name) VALUES ('Hühnerfleisch');
 INSERT INTO zutat (name) VALUES ('Schweinefleisch');
 INSERT INTO zutat (name) VALUES ('Honig');
 INSERT INTO zutat (name) VALUES ('Speck');
-INSERT INTO zutat (name) VALUES ('Marmelade');
+INSERT INTO zutat (name) VALUES ('Rinderhackfleisch');
+INSERT INTO zutat (name) VALUES ('Schweinehackfleisch');
+INSERT INTO zutat (name) VALUES ('Vollkornmehl');
+INSERT INTO zutat (name) VALUES ('Tomatenmark');
+INSERT INTO zutat (name) VALUES ('Knoblauch');
+INSERT INTO zutat (name) VALUES ('Olivenöl');
+INSERT INTO zutat (name) VALUES ('Spaghetti');
+INSERT INTO zutat (name) VALUES ('Ravioli');
+INSERT INTO zutat (name) VALUES ('Tortellini');
+INSERT INTO zutat (name) VALUES ('Nocken');
+INSERT INTO zutat (name) VALUES ('Rigatoni');
+INSERT INTO zutat (name) VALUES ('Tagliatelle');
 
 /* Insert Befehle für die Rezepte 
-
-INSERT INTO rezept (name, art, beschreibung) VALUES ('', '', '')
-
+(https://www.chefkoch.de/rezepte/1203251226129960/Schnelle-Bolognese-Sauce.html)
+INSERT INTO rezept (name, art, beschreibung) VALUES 
+    ('Spaghetti Bolognese', 'Mittagessen', 'Olivenöl in einem Topf erhitzen und das Hackfleisch darin krümelig braten. Die Zwiebel und die Knoblauchzehe klein würfeln und kurz mit braten. Das Tomatenmark hinzugeben und kurz anrösten. Nun den Rotwein hinzugeben und einkochen lassen. Die Dosentomaten, die Kräuter, das Paprikapulver und den Zucker hinzugeben und mit Salz, Pfeffer und gekörnter Brühe abschmecken. Den Deckel aufsetzen und die Sauce ca. 20 Minuten bei schwacher Hitze köcheln lassen. In der Zwischenzeit kann man die Spaghetti kochen und den Parmesan reiben.')
+    INNER JOIN ...
 */
